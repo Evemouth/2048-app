bcc2b827dd6a066bbf46849cb1ffebeb85973f91
Eve TURCHET <eturchet@bordeaux-inp.fr>
2026-01-27 11:27:42 +0100

    feat: add pipeline script (steps 1 and 2) and update dependencies in package.json

---
+++ pipelines/pipeline.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e # If error, exit immediately
+
+# Colors
+GREEN="\033[0;32m"
+BLUE="\033[0;34m"
+YELLOW="\033[1;33m"
+NC="\033[0m"
+
+# Script start
+echo ""
+echo -e "${BLUE}=================================${NC}"
+echo -e "${BLUE}        Starting Pipeline        ${NC}"
+echo -e "${BLUE}=================================${NC}"
+echo ""
+
+echo -e "${YELLOW}Step 1: Installing dependencies from cache...${NC}"
+pnpm install
+rm -rf node_modules
+pnpm install
+echo -e "${GREEN}[✓] Dependencies installed${NC}" 
+echo ""
+
+echo -e "${YELLOW}Step 2: Type checking...${NC}"
+pnpm nuxt typecheck # pnpm vue-tsc --noEmit ne fonctionne pas ?
+echo -e "${GREEN}[✓] Type checking passed${NC}"
+echo ""
+
+echo -e "${BLUE}=================================${NC}"
+echo -e "${BLUE} Pipeline Completed Successfully ${NC}"
+echo -e "${BLUE}=================================${NC}"
